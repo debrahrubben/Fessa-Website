7687697422840cb1392bb96ca8177b952fdb66a5
Rubben Debrah <64004519+debrahrubben@users.noreply.github.com>
2023-12-09 12:32:09 -0800

    gitignore config

--- my-vue-app/vite.config.js
+++ my-vue-app/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),],
+  build: {
+    outDir: 'dist', // Ensure this matches your Netlify publish directory
+  },
 })
