aef4c4fe210ca340d8280d5c5c249546349a91ca
Rubben Debrah <64004519+debrahrubben@users.noreply.github.com>
2023-12-23 13:47:19 -0800

    hmmm

--- my-vue-app/src/assets/Components/HallOfFamePage.jsx
+++ my-vue-app/src/assets/Components/HallOfFamePage.jsx
@@ -10,160 +10,138 @@ import Nartey from '../images/HallofFameImages/Naomi Nartey Mateykie.jpg'
 import Shadrack from '../images/HallofFameImages/Shadrack.jpg'
 import Rubben from '../images/HallofFameImages/Rubben.jpg'
 import Grace from '../images/HallofFameImages/Grace Abena Bossman.jpg'
+import wocom1 from '../images/HallofFameImages/wocom1.jpg'
+import wocom2 from '../images/HallofFameImages/wocom2.jpg'
+import wocom3 from '../images/HallofFameImages/wocom3.jpg'
+import academic from '../images/HallofFameImages/academic.jpg'
+import judiciary from '../images/HallofFameImages/judiciary.jpg'
+import electorial from '../images/HallofFameImages/electorial.jpg'
+import electorial2 from '../images/HallofFameImages/electorial2.jpg'
+import electorial3 from '../images/HallofFameImages/electorial3.jpg'
+import sports from '../images/HallofFameImages/sports.jpg'
+import sports2 from '../images/HallofFameImages/sports2.jpg'
+import publicrelation from '../images/HallofFameImages/public.jpg'
+import sponsorship from '../images/HallofFameImages/sponsorship.jpg'
+import christian from '../images/HallofFameImages/christian.jpg'
+import islamic from '../images/HallofFameImages/islamic.jpg'
 
-import { experimentalStyled as styled } from '@mui/material/styles';
-import { Box, Grid, Paper, } from '@mui/material';
 
-const HallOfFamePage = () => {
-  const Item = styled(Paper)({
-    backgroundColor: 'rgb(17, 50, 91)',  // Set the background color directly
-    textAlign: 'center',
-    color: 'white',
-    minWidth: '160px',
-    maxWidth: '310px',
-    width: '100%',
-    fontSize: '10px',
-    margin:'0px 0px 15px 35px',
-    padding:'3px'
+
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function ListBlockItem({ imgSrc, title, discount }) {
+  return (
+    <div className="list-block-item">
+      <img src={imgSrc} className="image-fit-contain" alt="img" />
+      <h6 className="title">{title}</h6>
+      <p className="discount"><strong>{discount}</strong></p>
+    </div>
+  );
+}
+
+function renderListItems(items) {
+  return items.map((item, index) => (
+    <div key={index} className="col-sm-4 col-6">
+      <ListBlockItem {...item} />
+    </div>
+  ));
+}
+
+// ... (imports and ListBlockItem component)
+function HallOfFamePage() {
+    const itemsData = [
+      { imgSrc:'', category: '2024/2025 Executives', Name: 'Dennis Asare', Position: 'President' },
+      { imgSrc: Ampah, category: '2024/2025 Executives', Name: 'Jeremiah Ampah', Position: 'Vice President' },
+      { imgSrc: Nartey, category: '2024/2025 Executives', Name: 'Naomi Nartey Mateykie', Position: 'General Secretary' },
+      { imgSrc: Osei, category: '2024/2025 Executives', Name: 'Frimpong Eric Osei', Position: 'Financial Secretary' },
+      { imgSrc: Bernice, category: '2024/2025 Executives', Name: 'Bernice Afi Agbanyo', Position: 'Wocom' },
+      { imgSrc: wocom1, category: '2024/2025 Executives', Name: '', Position: '' },
+      { imgSrc: wocom2, category: '2024/2025 Executives', Name: '', Position: '' },
+      { imgSrc: wocom3, category: '2024/2025 Executives', Name: '', Position: '' },
+      { imgSrc: academic, category: '2024/2025 Executives', Name: '', Position: '' },
+      { imgSrc: judiciary, category: '2024/2025 Executives', Name: '', Position: '' },
+      { imgSrc: electorial, category: '2024/2025 Executives', Name: '', Position: '' },
+      { imgSrc: electorial2, category: '2024/2025 Executives', Name: '', Position: '' },
+      { imgSrc: electorial3, category: '2024/2025 Executives', Name: '', Position: '' },
+      { imgSrc: sports, category: '2024/2025 Executives', Name: '', Position: '' },
+      { imgSrc: sports2, category: '2024/2025 Executives', Name: '', Position: '' },
+      { imgSrc: publicrelation, category: '2024/2025 Executives', Name: '', Position: '' },
+      { imgSrc: sponsorship, category: '2024/2025 Executives', Name: '', Position: '' },
+      { imgSrc: christian, category: '2024/2025 Executives', Name: '', Position: '' },
+      { imgSrc: islamic, category: '2024/2025 Executives', Name: '', Position: '' },
+      { imgSrc: '', category: '2024/2025 Executives', Name: '', Position: '' },
+      { imgSrc: 'https://www.bootdey.com/image/320x320/FF8C00/000000', category: '2022/2023 Executives', Name: 'Modern Chair', Position: 55 },
+      { imgSrc: 'https://www.bootdey.com/image/320x320/8B008B/000000', category: '2021/2022 Executives', Name: 'Elegant Chair', Position: 60 },
+      { imgSrc: 'https://www.bootdey.com/image/320x320/00CED1/000000', category: 'Lecturers', Name: 'Cozy Sofa', Position: 70 },
+      // Add more categories and items as needed
+    ];
+  
+    const categoryItemsMap = {};
+
+  itemsData.forEach((item) => {
+    const { category, Name } = item;
+
+    if (!categoryItemsMap[category]) {
+      categoryItemsMap[category] = [];
+    }
+
+    categoryItemsMap[category].push({ Name, imgSrc: item.imgSrc, Position: item.Position });
   });
 
-  const cardData = [
-    {
-      id: 1,
-      imageSrc: Asare,
-      quote: "Sometimes I think the surest sign that intelligent life exists elsewhere in the universe is that none of it has tried to contact us.",
-      name: 'Dennis Asare',
-      role: 'President',
-    },
-    {
-      id: 2,
-      imageSrc: Ampah,
-      quote: "Sometimes I think the surest sign that intelligent life exists elsewhere in the universe is that none of it has tried to contact us.",
-      name: 'Jeremiah Ampah',
-      role: 'Vice President',
-    },
-    {
-      id: 3,
-      imageSrc: Nartey,
-      quote: "Sometimes I think the surest sign that intelligent life exists elsewhere in the universe is that none of it has tried to contact us.",
-      name: 'Naomi Nartey Mateykie',
-      role: 'General Secretary',
-    },
-    {
-      id: 4,
-      imageSrc: Osei,
-      quote: "Sometimes I think the surest sign that intelligent life exists elsewhere in the universe is that none of it has tried to contact us.",
-      name: 'Frimpong Eric Osei',
-      role: 'Financial Secretary',
-    },
-    {
-      id: 5,
-      imageSrc: Umar,
-      quote: "Sometimes I think the surest sign that intelligent life exists elsewhere in the universe is that none of it has tried to contact us.",
-      name: 'Mohammed Umar Nadra',
-      role: 'Organising Secretary',
-    },
-    {
-      id: 6,
-      imageSrc: Bernice,
-      quote: "Sometimes I think the surest sign that intelligent life exists elsewhere in the universe is that none of it has tried to contact us.",
-      name: 'Bernice Afi Agbanyo',
-      role: 'Wocom',
-    },
-    {
-      id: 7,
-      imageSrc: Kerker,
-      quote: "Sometimes I think the surest sign that intelligent life exists elsewhere in the universe is that none of it has tried to contact us.",
-      name: 'Christian Kerker',
-      role: 'Ex President 2023',
-    },
-    {
-      id: 8,
-      imageSrc: Mintah,
-      quote: "Sometimes I think the surest sign that intelligent life exists elsewhere in the universe is that none of it has tried to contact us.",
-      name: 'Kenseng Mintah',
-      role: 'Ex Vice President 2023',
-    },
-    {
-      id: 9,
-      imageSrc: Bonsu,
-      quote: "Sometimes I think the surest sign that intelligent life exists elsewhere in the universe is that none of it has tried to contact us.",
-      name: 'Kwame Bonsu',
-      role: 'Ex President 2022',
-    },
-    {
-      id: 10,
-      imageSrc: Grace,
-      quote: "Sometimes I think the surest sign that intelligent life exists elsewhere in the universe is that none of it has tried to contact us.",
-      name: 'Grace Abena Bossman',
-      role: 'General Secretary 2022',
-    },
-    {
-      id: 11,
-      imageSrc: Rubben,
-      quote: "Sometimes I think the surest sign that intelligent life exists elsewhere in the universe is that none of it has tried to contact us.",
-      name: 'Rubben Debrah',
-      role: 'Fessa Web Developer',
-    },
-    {
-      id: 12,
-      imageSrc: Shadrack,
-      quote: "Sometimes I think the surest sign that intelligent life exists elsewhere in the universe is that none of it has tried to contact us.",
-      name: 'Duah Shadrack Amponsah',
-      role: 'Fessa Web Master',
-    },
-
-    // Add more card data as needed
-  ];
-
- 
-  const renderGridAndHeaders = (startIndex, endIndex, headerText) => (
-    <Box >
-      <h2 style={{ textAlign: 'center', color:'#bcc4d6',backdropFilter: 'blur(5px)', padding:'2px 0px 40px 0px' }}>{headerText}</h2>
-      <Grid container spacing={4} columns={{ xs: 4, sm: 8, md: 12 }} >
-        {cardData.slice(startIndex, endIndex).map((card) => (
-          <Grid xs={2} sm={4} md={4} key={card.id} style={{ display: 'flex', justifyContent: 'center', position: 'relative',  }}>
-            <Item>
-              <div style={{ position: 'relative', borderBottom: '2px solid #34495e', zIndex: '1', }}>
-                <img
-                  src={card.imageSrc}
-                  alt={`sample${card.id}`}
-                  style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-                />
-                <figcaption style={{ backgroundColor: 'rgb(17, 50, 91)', padding: '01px' }}>
-                  <h3
-                    style={{
-                      margin: '0 0 5px',
-                      textTransform: 'uppercase',
-                      fontWeight: '400',
-                      textAlign: 'center',
-                      marginTop: '10px',
-                      marginBottom: '10px',
-                      color: 'white',
-                      fontSize: 'small',
-                    }}
-                  >
-                    {card.name}
-                    <br />
-                    <span style={{ fontWeight: '700' }}> {card.role}</span>
-                  </h3>
-                  <p style={{ marginTop: '5px', color: 'white' }}>{card.quote}</p>
-                </figcaption>
-              </div>
-            </Item>
-          </Grid>
-        ))}
-      </Grid>
-    </Box>
-  );
+  const uniqueCategories = Object.keys(categoryItemsMap);
 
   return (
-    <Box sx={{ flexGrow: 1, backgroundImage: 'url("https://images.unsplash.com/photo-1651437524278-b37b83a6e6d3?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")', paddingTop: '95px', }}>
-      {renderGridAndHeaders(0, 6, 'Executives')}
-      {renderGridAndHeaders(6, 8, 'Predecessors')}
-      {renderGridAndHeaders(10, cardData.length, 'Remarkable People')}
-    </Box>
+    <div style={{ paddingTop: '90px' }}>
+      <div className="container">
+        <div className="row accordion" id="accordion">
+          {uniqueCategories.map((category, index) => (
+            <div key={index} className="col-xl-6">
+              <div className="row">
+                <div className="col-lg-12">
+                  <div className="card accordion-item product_list">
+                    <div className="card-header accordion-header">
+                      <div
+                        className="btn btn-link accordion-button"
+                        data-bs-toggle="collapse"
+                        data-bs-target={`#item${index + 1}`}
+                        aria-expanded={index === 0 ? 'true' : 'false'}
+                      >
+                        <div className="list_image">
+                          <img
+                            src="https://www.bootdey.com/image/320x320/FF69B4/000000"
+                            className="image-fit-contain"
+                            alt="category-img"
+                          />
+                        </div>
+                        <h5 className="title">{category}</h5>
+                      </div>
+                    </div>
+                    <div
+                      id={`item${index + 1}`}
+                      className={`accordion-collapse collapse ${index === 0 ? 'show' : ''}`}
+                      data-bs-parent="#accordion"
+                    >
+                      <div className="card-body">
+                        <div className="row">
+                          {categoryItemsMap[category].map((item, itemIndex) => (
+                            <div key={itemIndex} className="col-sm-6 col-12">
+                              <ListBlockItem imgSrc={item.imgSrc} title={item.Name} discount={item.Position} />
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
-};
+}
 
 export default HallOfFamePage;
