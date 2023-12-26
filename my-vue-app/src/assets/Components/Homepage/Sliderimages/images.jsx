5ad2904ac3bfeab5cc65b65b2b731a68d4443cdd
Rubben Debrah <64004519+debrahrubben@users.noreply.github.com>
2023-12-26 10:08:30 -0800

    Update images.jsx

--- my-vue-app/src/assets/Components/Homepage/Sliderimages/images.jsx
+++ my-vue-app/src/assets/Components/Homepage/Sliderimages/images.jsx
@@ -1,6 +1,14 @@
 
 
 const images = [
+  {
+    imgURL: 'https://res.cloudinary.com/dgpxvazru/image/upload/v1703585192/Slide/IMG-20231225-WA0006_hfveoq.jpg',
+    imgAlt: "details-slide-6"
+  },
+  {
+    imgURL: 'https://res.cloudinary.com/dgpxvazru/image/upload/v1703585190/Slide/IMG-20231225-WA0017_e3yrfq.jpg',
+    imgAlt: "details-slide-5"
+  },
     {
       imgURL: 'https://res.cloudinary.com/dgpxvazru/image/upload/v1703384591/Slide/details-slide-1_koxegc.jpg',
       imgAlt: "details-slide-1"
