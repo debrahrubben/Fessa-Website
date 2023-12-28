5730e23b33297e040d9152ab91f9964669dead17
Rubben Debrah <64004519+debrahrubben@users.noreply.github.com>
2023-12-28 02:05:53 -0800

    floating button

--- my-vue-app/src/assets/Components/Homepage/Externalsite.jsx
+++ my-vue-app/src/assets/Components/Homepage/Externalsite.jsx
@@ -1,11 +1,16 @@
-import { useState } from 'react';
+import  { useState } from 'react';
 import { Button, Modal } from 'antd';
+import { FloatButton } from 'antd';
+import { BookOutlined } from '@ant-design/icons';
+
 const Externalsite = () => {
   const [loading, setLoading] = useState(false);
   const [open, setOpen] = useState(false);
+
   const showModal = () => {
     setOpen(true);
   };
+
   const handleOk = () => {
     setLoading(true);
     setTimeout(() => {
@@ -13,44 +18,69 @@ const Externalsite = () => {
       setOpen(false);
     }, 3000);
   };
+
   const handleCancel = () => {
     setOpen(false);
   };
+
   return (
     <>
-      <Button type="primary" onClick={showModal}>
+      <FloatButton type="primary" onClick={showModal} 
+      icon={<BookOutlined />}
+    
+      style={{ right: 54,}}>
+      <span className="material-icons">
+school
+</span>
         Access Student Portal / Virtual classroom
-      </Button>
+      </FloatButton>
       <Modal
-        open={open}
-        title="Title"
+        visible={open} // Use "visible" instead of "open"
         onOk={handleOk}
         onCancel={handleCancel}
         footer={[
           <Button key="back" onClick={handleCancel}>
             Return
           </Button>,
-          <Button key="submit" type="primary" loading={loading} onClick={handleOk}>
-            Submit
-          </Button>,
           <Button
-            key="link"
-            href="https://google.com"
+            key="link-portal"
+            href="https://apps.knust.edu.gh/students"
             type="primary"
             loading={loading}
             onClick={handleOk}
           >
-            Search on Google
+            Student Portal
+          </Button>,
+          <Button
+            key="link-classroom"
+            href="https://myclass.knust.edu.gh/login/index.php"
+            type="primary"
+            loading={loading}
+            onClick={handleOk}
+          >
+            Virtual Classroom
           </Button>,
         ]}
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <div style={{ display: 'flex' }}>
+          <div style={{ flex: 1, marginRight: '10px' }}>
+            <img
+              src="https://i.pinimg.com/originals/23/45/c5/2345c5c02303b4e1373f5a7aa81ef1b9.png"
+              alt=""
+              style={{ width: '100%', height:'100%', }}
+            />
+          </div>
+          <div style={{ flex: 1 }}>
+            <img
+              src="https://cdn.mfidie.com/wp-content/uploads/2022/08/20220816_110209.jpg"
+              alt=""
+              style={{ width: '100%' }}
+            />
+          </div>
+        </div>
       </Modal>
     </>
   );
 };
+
 export default Externalsite;
