39a29924b37a4ebcfbb6064ec0218b09c1cfe8e1
Rubben Debrah <64004519+debrahrubben@users.noreply.github.com>
2023-12-28 19:58:40 -0800

    fes button

--- my-vue-app/src/assets/Components/Homepage/Externalsite.jsx
+++ my-vue-app/src/assets/Components/Homepage/Externalsite.jsx
@@ -25,7 +25,7 @@ const Externalsite = () => {
 
   return (
     <>
-      <FloatButton type="primary" onClick={showModal} icon={<BookOutlined />} style={{ right: 54 }}>
+      <FloatButton type="primary" onClick={showModal} icon={<BookOutlined />} >
         <span className="material-icons">school</span> Access Student Portal / Virtual classroom
       </FloatButton>
       <Modal 
@@ -33,12 +33,13 @@ const Externalsite = () => {
         onOk={handleOk}
         onCancel={handleCancel} 
         footer={ [
-          <Button
+          <Button 
             key="link-portal"
             href="https://apps.knust.edu.gh/students"
             type="primary"
             loading={loading}
             onClick={handleOk}
+            style={{backgroundColor:'#17223A'}}
           >
             Student Portal
           </Button>,
@@ -48,6 +49,7 @@ const Externalsite = () => {
             type="primary"
             loading={loading}
             onClick={handleOk}
+            style={{backgroundColor:'#17223A'}}
           >
             Virtual Classroom
           </Button>,
@@ -55,18 +57,18 @@ const Externalsite = () => {
       >
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
           <div style={{ flex: 1, marginRight: '10px' }}>
-            <img
+           <a href="https://apps.knust.edu.gh/students"> <img
               src="https://i.pinimg.com/originals/23/45/c5/2345c5c02303b4e1373f5a7aa81ef1b9.png"
               alt=""
               style={{ width: '100%', height: '100%' }}
-            />
+            /></a>
           </div>
           <div style={{ flex: 1 }}>
-            <img
+           <a href="https://myclass.knust.edu.gh/login/index.php"> <img
               src="https://cdn.mfidie.com/wp-content/uploads/2022/08/20220816_110209.jpg"
               alt=""
               style={{ width: '100%' }}
-            />
+            /></a>
           </div>
         </div>
       </Modal>
