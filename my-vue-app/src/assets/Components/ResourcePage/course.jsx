6a444a0869a98692c43bd006e5d22ae107b7d397
Rubben Debrah <64004519+debrahrubben@users.noreply.github.com>
2024-01-20 07:29:23 -0800

    Update course.jsx

--- my-vue-app/src/assets/Components/ResourcePage/course.jsx
+++ my-vue-app/src/assets/Components/ResourcePage/course.jsx
@@ -5,8 +5,8 @@ const Course = () => {
  
   return (
     <div style={{paddingTop:'90px', backgroundColor:'rgb(17, 50, 91)',minHeight:'100vh'}}>
-      <h1 style={{ textAlign: 'center', marginTop: '30px', marginBottom: '20px', color:'white', fontSize:'small' }}>
-        The red Pin mark`s the faculty location
+      <h1 style={{ textAlign: 'center', marginTop: '3px', marginBottom: '20px', color:'white', fontSize:'small' }}>
+        Choose the course
       </h1>
     </div>
   );
