17ad06b3b391073a46409b7af3496e2763d6e520
Rubben Debrah <64004519+debrahrubben@users.noreply.github.com>
2024-02-12 08:11:53 +0000

    Update images.jsx

--- my-vue-app/src/assets/Components/Homepage/slidercomponents/images.jsx
+++ my-vue-app/src/assets/Components/Homepage/slidercomponents/images.jsx
@@ -2,8 +2,12 @@
 
 const images = [
   {
-    imgURL: 'https://res.cloudinary.com/dgpxvazru/image/upload/v1705137100/Slide/newslide1_pd1t4m.jpg',
-    imgAlt: "details-slide-7"
+    imgURL: 'https://res.cloudinary.com/dgpxvazru/image/upload/v1707725270/Slide/photo_2024-02-12_08-02-11_s3olrb.webp',
+    imgAlt: "details-slide-9"
+  },
+  {
+    imgURL: 'https://res.cloudinary.com/dgpxvazru/image/upload/v1707725270/Slide/photo_2024-02-12_08-02-15_ip6sqj.webp',
+    imgAlt: "details-slide-8"
   },
   {
     imgURL: 'https://res.cloudinary.com/dgpxvazru/image/upload/v1703585192/Slide/IMG-20231225-WA0006_hfveoq.jpg',
@@ -13,10 +17,6 @@ const images = [
     imgURL: 'https://res.cloudinary.com/dgpxvazru/image/upload/v1703585190/Slide/IMG-20231225-WA0017_e3yrfq.jpg',
     imgAlt: "details-slide-5"
   },
-    {
-      imgURL: 'https://res.cloudinary.com/dgpxvazru/image/upload/v1703384591/Slide/details-slide-3_buydm4.jpg',
-      imgAlt: "details-slide-3"
-    },
     {
       imgURL: 'https://res.cloudinary.com/dgpxvazru/image/upload/v1703384592/Slide/details-slide-2_qwtl7d.jpg',
       imgAlt: "details-slide-2"
