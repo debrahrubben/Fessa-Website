ef1d8dc254588d99e11a8cdcd4b453f9ae2ad2f9
Rubben Debrah <64004519+debrahrubben@users.noreply.github.com>
2024-02-15 13:34:16 +0000

    Update HomePage.jsx

--- my-vue-app/src/assets/Components/Homepage/HomePage.jsx
+++ my-vue-app/src/assets/Components/Homepage/HomePage.jsx
@@ -2,7 +2,8 @@ import Header from './Header'
 import { Suspense } from 'react';
 import { lazy } from "react";
 const NewsTimeline = lazy(() => import("./News"));
-const ImageSlider = lazy(()=> import('./slidercomponents/ImageSlider'));
+const ImageSlider = lazy(()=> import('./slidercomponents/ImageSlider')); 
+import { Skeleton } from 'antd';
 
 const HomePage = () => {
   return (
@@ -10,12 +11,12 @@ const HomePage = () => {
          <Header />
       <div className="outer-container" style={{backgroundColor:'rgb(17, 50, 91)'}} >
         <div className="slider-container">
-        <Suspense fallback={<h1>Loading . . .</h1>}>
+        <Suspense fallback={<Skeleton.Image active />}>
           <ImageSlider /> 
           </Suspense>
         </div>
       </div>
-      <Suspense fallback={<h1>Loading . . .</h1>}>
+      <Suspense fallback={<Skeleton.Image active  paragraph={{ rows: 4,}} />}>
       <NewsTimeline />
       </Suspense>
    
