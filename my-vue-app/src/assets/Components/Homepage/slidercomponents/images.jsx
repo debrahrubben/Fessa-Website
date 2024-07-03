71b4287422cb44d7d9ea08b16ab774ec0b7c733a
Rubben Debrah <64004519+debrahrubben@users.noreply.github.com>
2024-07-03 17:43:38 +0000

    Update images.jsx

--- my-vue-app/src/assets/Components/Homepage/slidercomponents/images.jsx
+++ my-vue-app/src/assets/Components/Homepage/slidercomponents/images.jsx
@@ -13,10 +13,7 @@ const images = [
     imgURL: 'https://res.cloudinary.com/dgpxvazru/image/upload/v1712222089/Slide/photo_2024-04-04_09-12-57_c9clog.webp',
     imgAlt: "boawonua"
   },
-  {
-    imgURL: 'https://res.cloudinary.com/dgpxvazru/image/upload/v1718225898/Slide/photo_2024-06-12_20-53-39_zlelxa.webp',
-    imgAlt: "career fair"
-  },
+
   {
     imgURL: 'https://res.cloudinary.com/dgpxvazru/image/upload/v1711659785/Slide/photo_2024-03-28_20-54-10_kidhxu.webp',
     imgAlt: "excursion"
